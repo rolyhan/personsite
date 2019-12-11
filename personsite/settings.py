8f73bc503ba2d620ad64de3e58505520c67053cd
hanz <1422031530@qq.com>
2019-12-11 14:57:37 +0800

    one day

--- personsite/settings.py
+++ personsite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app01',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'personsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'personsite',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '192.168.239.128',
+        'OPTIONS': {'isolation_level': None},
     }
 }
 
